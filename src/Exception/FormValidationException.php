02f070a30d65b46c4fd0e9c6a2552cc99655da8a
Derek Stephen McLean <delboy1978uk@gmail.com>
2025-03-14 18:32:23 +0100

    update exception

--- src/Exception/FormValidationException.php
+++ src/Exception/FormValidationException.php
@@ -4,4 +4,13 @@ declare(strict_types=1);
 
 namespace Del\Form\Exception;
 
-class FormValidationException {}
+use Bone\Exception;
+
+class FormValidationException extends Exception
+{
+    public function __construct(array $errors)
+    {
+        $message = \implode(', ', $errors);
+        parent::__construct($message, 400);
+    }
+}
