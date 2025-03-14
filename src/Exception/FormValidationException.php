ff6e33bc95ecbc80989538aa1133cd8e4dc48983
Derek Stephen McLean <delboy1978uk@gmail.com>
2025-03-14 18:33:14 +0100

    add getter

--- src/Exception/FormValidationException.php
+++ src/Exception/FormValidationException.php
@@ -8,9 +8,17 @@ use Bone\Exception;
 
 class FormValidationException extends Exception
 {
+    private array $errors;
+
     public function __construct(array $errors)
     {
+        $this->errors = $errors;
         $message = \implode(', ', $errors);
         parent::__construct($message, 400);
+    }
+
+    public function getErrors(): array
+    {
+        return $this->errors;
     }
 }
